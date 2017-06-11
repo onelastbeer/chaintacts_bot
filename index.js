8243c30593252b26637a1fe27f592d938a7aefdb
cyclohexanol <ambroise.mean@gmail.com>
2017-06-11 16:22:14 +0200

    Init

--- index.js
+++ index.js
@@ -1,33 +1,7 @@
-var express = require('express');
-var app = express();
+var TelegramBot = require('node-telegram-bot-api');
+var token = 'CHAINTACT_BOT_TOKEN';
 
-app.set('port', (process.env.PORT || 5000));
-app.get('/', function(request, response) {
-
+var bot = new TelegramBot(token, {polling: true});
+bot.getMe().then(function (me) {
+  console.log('Hi my name is %s!', me.username);
 });
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
-
-const Telegram = require('telegram-node-bot')
-const TelegramBaseController = Telegram.TelegramBaseController
-const tg = new Telegram.Telegram('CHAINTACT_BOT_TOKEN')
-
-class GreetingController extends TelegramBaseController {
-  /**
-   * @param {Scope} $
-   */
-    greetingHandler($) {
-        $.sendMessage('Hey, how are you?')
-    }
-get routes() {
-        return {
-            'hey': 'greetingHandler',
-            'hi': 'greetingHandler',
-            'hello': 'greetingHandler',
-        }
-    }
-}
-
-tg.router
-  .when(['hey', 'hi', 'hello'], new GreetingController())
