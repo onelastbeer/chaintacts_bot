5927901754c904555dd45f57b1f9c6323b8086ba
cyclohexanol <ambroise.mean@gmail.com>
2017-06-11 16:50:17 +0200

    Added messages to /start and coming soon

--- index.js
+++ index.js
@@ -9,15 +9,19 @@ bot.getMe().then(function (me) {
 //matches /start
 bot.onText(/\/start/, function (msg, match) {
   var fromId = msg.from.id; // get the id, of who is sending the message
-  var message = "Welcome to your WeatherBot\n"
-  message += "Get weather update by sending /weather [your_city] command."
+  var message = "Welcome to your ChaintactBot\n"
+  message += "Commands:"
+  message += "/chaintact help - List of commands"
+  message += "/chaintact set [ETH address] - Set your ETH address to your profil"
+  message += "/chaintact get [telegram user] - Sends this user ETH address"
+  message += "/chaintact group - Sends the list of all group users ETH addresses"
   bot.sendMessage(fromId, message);
 });
 
 //match /weather [whatever]
-bot.onText(/\/weather (.+)/, function (msg, match) {
+bot.onText(/\/chaintact (.+)/, function (msg, match) {
   var fromId = msg.from.id; // get the id, of who is sending the message
   var postcode = match[1];
-  var message = "We will look for weather on postcode "+ postcode;
+  var message = "Coming soon"
   bot.sendMessage(fromId, message);
 });
