63c93a9dca6eb57588a4b551c2ca7d18b8f6bb21
cyclohexanol <ambroise.mean@gmail.com>
2017-06-11 16:27:37 +0200

    Init

--- index.js
+++ index.js
@@ -5,3 +5,19 @@ var bot = new TelegramBot(token, {polling: true});
 bot.getMe().then(function (me) {
   console.log('Hi my name is %s!', me.username);
 });
+
+//matches /start
+bot.onText(/\/start/, function (msg, match) {
+  var fromId = msg.from.id; // get the id, of who is sending the message
+  var message = "Welcome to your WeatherBot\n"
+  message += "Get weather update by sending /weather [your_city] command."
+  bot.sendMessage(fromId, message);
+});
+
+//match /weather [whatever]
+bot.onText(/\/weather (.+)/, function (msg, match) {
+  var fromId = msg.from.id; // get the id, of who is sending the message
+  var postcode = match[1];
+  var message = "We will look for weather on postcode "+ postcode;
+  bot.sendMessage(fromId, message);
+});
