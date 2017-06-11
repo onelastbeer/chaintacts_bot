6f41516ec61c3587870191bc5449c889ae377f59
cyclohexanol <ambroise.mean@gmail.com>
2017-06-11 16:35:34 +0200

    Init

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var TelegramBot = require('node-telegram-bot-api');
-var token = 'CHAINTACT_BOT_TOKEN';
-
+var token = 'process.env.CHAINTACT_BOT_TOKEN';
 var bot = new TelegramBot(token, {polling: true});
+
+
 bot.getMe().then(function (me) {
   console.log('Hi my name is %s!', me.username);
 });
